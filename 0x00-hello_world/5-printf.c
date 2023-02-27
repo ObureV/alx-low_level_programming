1d6361ec09df76fea19734b13d65d12d27229ea9
ObureV <oburevin@gmail.com>
2023-02-26 21:24:19 -0800

    using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
  * main - Program that prints using printf
  * return: 0 (Success)
  */
-init main(void)
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
